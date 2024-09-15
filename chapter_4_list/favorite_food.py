b25f354d86398295e39cdd7befbde103b579f839
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2024-09-15 20:13:40 -0300

    Copying a list
    Creating  a new list from an existing one using slice

---
+++ chapter_4_list/favorite_food.py
@@ -0,0 +1,19 @@
+my_foods = ['Pizza', 'Cake', 'Fries']
+
+my_friends_foods = my_foods[:]
+
+print("My favorite foods are:")
+print(my_foods)
+
+print("My favorite friends food are:")
+print(my_friends_foods)
+
+my_foods.append('Chocolate')
+
+my_friends_foods.append('Milkshake')
+
+print("My favorite foods are:")
+print(my_foods)
+
+print("My favorite friends food are:")
+print(my_friends_foods)
