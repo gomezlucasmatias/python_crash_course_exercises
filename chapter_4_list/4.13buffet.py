82a86158d90c180bb3088987de70f421730e3acf
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2024-09-18 10:10:01 -0300

    Modifying a tuple

---
+++ chapter_4_list/4.13buffet.py
@@ -0,0 +1,14 @@
+buffer_menu = ('eggs and bakon', 'toast and juicy', 'Carbonara pasta', 'roast chiken', 'betabel soup')
+
+print('The Buffer Styly offer you: ')
+for dish in buffer_menu:
+    print(dish.title())
+
+# buffer_menu[0] = ('Cereal with milk')
+
+buffer_menu = ('eggs and bakon', 'cereal with milk', 'Carbonara pasta', 'Bread bowl chiken', 'betabel soup')
+
+print('The Buffer Styly offer you: ')
+for dish in buffer_menu:
+    print(dish.title())
+
