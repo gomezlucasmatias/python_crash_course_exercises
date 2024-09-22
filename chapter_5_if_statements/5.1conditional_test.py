bd3e9bd0881f4b29be74c4bd2c5dde763dde3316
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2024-09-22 20:44:48 -0300

    Improve readability

--- chapter_5_if_statements/5.1conditional_test.py
+++ chapter_5_if_statements/5.1conditional_test.py
@@ -6,28 +6,28 @@ print('\n Is fruit == "Tomatoe"? I predict False')
 print(fruit == 'Tomatoe')
 
 teg_country = 'Kamchanka'
-print("Is teg_country == 'Kamchanka'? I predict True")
+print("\nIs teg_country == 'Kamchanka'? I predict True")
 print(teg_country == 'Kamchanka')
 
 print('\n Is teg_country == "Java"? I predict False')
 print(teg_country == 'Java')
 
 mate = 'Amargo'
-print("Is mate == 'Amargo'? I predict True")
+print("\nIs mate == 'Amargo'? I predict True")
 print(mate == 'Amargo')
 
 print('\n Is mate == "Dulce"? I predict False')
 print(mate == 'Dulce')
 
 post_punk = 'Joy Division'
-print('Is post_punk == "Joy Division"? I predict True')
+print('\nIs post_punk == "Joy Division"? I predict True')
 print(post_punk == 'Joy Division')
 
 print('\n Is post_punk == "Madonna"? I predict False')
 print(post_punk == 'Madonna')
 
 liga = 'Premier League'
-print('Is liga == "Premier League"? I predict True')
+print('\nIs liga == "Premier League"? I predict True')
 print(liga == 'Premier League')
 
 print('\n Is liga == "La Liga"? I predict False')
