2c0c04c29b5d4dc0464be54f829cbae0ee113ab4
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2024-09-22 20:37:42 -0300

    If statements practice
    Write a small programme to practise the mechanics of the If statement
    and Boolean expressions.

---
+++ chapter_5_if_statements/5.1conditional_test.py
@@ -0,0 +1,34 @@
+fruit = 'banana'
+print('Is fruit == "banana"? I predict True')
+print(fruit == 'banana')
+
+print('\n Is fruit == "Tomatoe"? I predict False')
+print(fruit == 'Tomatoe')
+
+teg_country = 'Kamchanka'
+print("Is teg_country == 'Kamchanka'? I predict True")
+print(teg_country == 'Kamchanka')
+
+print('\n Is teg_country == "Java"? I predict False')
+print(teg_country == 'Java')
+
+mate = 'Amargo'
+print("Is mate == 'Amargo'? I predict True")
+print(mate == 'Amargo')
+
+print('\n Is mate == "Dulce"? I predict False')
+print(mate == 'Dulce')
+
+post_punk = 'Joy Division'
+print('Is post_punk == "Joy Division"? I predict True')
+print(post_punk == 'Joy Division')
+
+print('\n Is post_punk == "Madonna"? I predict False')
+print(post_punk == 'Madonna')
+
+liga = 'Premier League'
+print('Is liga == "Premier League"? I predict True')
+print(liga == 'Premier League')
+
+print('\n Is liga == "La Liga"? I predict False')
+print(liga == 'La Liga')
