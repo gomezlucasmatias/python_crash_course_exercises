f7347a205941e50f13c1e763335e65d86a281889
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2024-09-27 23:09:40 -0300

    Conditional test
    Write a simple programme that performs simple tests to compare.

---
+++ chapter_5_if_statements/5.2more_conditional_test.py
@@ -0,0 +1,30 @@
+# Tets for equality and inequality with strings
+name = 'matias'
+print('Is name == "matias"?')
+print(name == 'matias')
+
+print('\nIs name != "matias"?')
+print(name != 'matias')
+
+#Test using the lower() method
+
+name = 'Ame'
+print('\nIs name "Ame"?')
+print(name == 'ame')
+print('\nIs name "ame"?')
+print(name.lower() == 'ame')
+
+# Numerical tests involving equality and inequality, greater than and less than,
+# greater or equal to, less or equal to.
+
+number_1 = 7
+number_2 = 3
+
+print('\nIs number_1 == number_2?')
+print(number_1 == number_2)
+
+print('\nIs number_1 == 7?')
+print(number_1 == 7)
+    
+print('\n Is number_1 > number_2?')
+print(number_1 > number_2)
