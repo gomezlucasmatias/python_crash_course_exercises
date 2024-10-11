b558aa4a6a989c6723019b5653d26a703448e1fe
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2024-10-10 23:04:40 -0300

    Add else statement

--- chapter_5_if_statements/voting.py
+++ chapter_5_if_statements/voting.py
@@ -2,3 +2,7 @@ age = 19
 
 if age >= 18:
     print('You are old enough to vote!')
+    print('Have you registered to vote yet?')
+else:
+    print('Sorry, you are too younge to vote')
+    print('Please register to vote as soon as you turn 18!')
