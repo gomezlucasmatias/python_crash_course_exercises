51fda7c5c214f8f61c5bfc26fd3223580b5b470e
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2024-10-14 23:40:28 -0300

    Add input for consult username

--- chapter_5_if_statements/banned_user.py
+++ chapter_5_if_statements/banned_user.py
@@ -1,6 +1,8 @@
-users = ['Vanina', 'Pablo', 'Paulina', 'Rosalia']
+banned_users = ['Vanina', 'Pablo', 'Paulina', 'Rosalia']
 
-if 'Vanina' in users:
-    print("You are banned from the forum, you can't comment")
+user = input("Login your usser: ")
+
+if user in banned_users:
+    print(f"{user} Can't  comment, {user} is banned")
 else:
-    print("You can comment")
+    print("You can comment in this forum")
