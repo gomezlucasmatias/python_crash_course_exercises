4260155fa5906e01b04b484133b59d9836b6e34e
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2024-10-15 23:43:33 -0300

    Practice if, elif, else blocks

---
+++ chapter_5_if_statements/5.4alien_colors_2.py
@@ -0,0 +1,12 @@
+alien_color = 'yellow'
+
+if alien_color == 'green':
+    print('You won 5 points')
+elif alien_color != 'green':
+    print('You won 10 points')
+
+if alien_color == 'green':
+    print('You won 5 points')
+else:
+    print('You won 10 points')
+
