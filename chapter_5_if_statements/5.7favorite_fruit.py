6d0a433180825432a64366f2867d5cb803f2d1df
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2024-10-16 01:05:11 -0300

    Use if statement to checks items in a list

---
+++ chapter_5_if_statements/5.7favorite_fruit.py
@@ -0,0 +1,10 @@
+favorite_fruits = ['Tangarine', 'Guayaba', 'Lemon']
+
+if 'Tangarine' in favorite_fruits:
+    print('Oh! You really like tangarines')
+if 'Water melon' in favorite_fruits:
+    print("Oh! You really like water melon")
+if 'Guayaba' in favorite_fruits:
+    print('Oh! You also like Guayaba')
+if 'Lemon' in favorite_fruits:
+    print('Oh! I really love lemon too')
