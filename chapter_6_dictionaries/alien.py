b4f96734d3705799bc4f6aaa9fc0e6dd8d079cb0
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2024-11-03 19:22:37 -0300

    Modifying displayed messages

--- chapter_6_dictionaries/alien.py
+++ chapter_6_dictionaries/alien.py
@@ -1,13 +1,13 @@
 alien_0 = {'color': 'green', 'points': 5}
 
-print(alien_0['color'])
-print(alien_0['points'])
+#print(alien_0['color'])
+#print(alien_0['points'])
 
 alien_0['x_position'] = 0
 alien_0['y_position'] = 25
 alien_0['speed'] = 'medium'
-print(alien_0)
-
+#print(alien_0)
+print(f'Original position: {alien_0["x_position"]}')
 if alien_0['speed'] == 'slow':
     increase_x = 1
     alien_0['x_position'] = alien_0['x_position'] + increase_x
