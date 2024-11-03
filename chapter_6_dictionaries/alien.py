73024715e9095c7b0bea9f8baa830c7ebfbd83cf
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2024-11-02 23:22:54 -0300

    Update dictionary key-value

--- chapter_6_dictionaries/alien.py
+++ chapter_6_dictionaries/alien.py
@@ -5,5 +5,18 @@ print(alien_0['points'])
 
 alien_0['x_position'] = 0
 alien_0['y_position'] = 25
-
+alien_0['speed'] = 'medium'
 print(alien_0)
+
+if alien_0['speed'] == 'slow':
+    increase_x = 1
+    alien_0['x_position'] = alien_0['x_position'] + increase_x
+elif alien_0['speed'] == 'medium':
+    increase_x = 2
+    alien_0['x_position'] = alien_0['x_position'] + increase_x 
+else:
+    increase_x = 3
+    alien_0['x_position'] = alien_0['x_position'] + increase_x 
+
+print(f'New position: {alien_0["x_position"]}')
+    
