f64c02a1df100073bd74bfd4a15542d1029149f3
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2024-11-03 20:08:06 -0300

    Using key-value from a dictionary

---
+++ chapter_6_dictionaries/favorite_languages.py
@@ -0,0 +1,10 @@
+favorite_lenguages = {
+        'mati': 'python',
+        'mauro': 'c#',
+        'betti': 'html',
+        'sabri': 'javascript',
+        }
+
+language = favorite_lenguages['mati'].title()
+
+print(f"Mati's favorite language is {language}")
