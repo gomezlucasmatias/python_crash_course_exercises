f55f9ee9f127e271a56e3c935d1f13fba478862c
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2024-11-05 17:46:27 -0300

    Add formatted print messages for the item list

---
+++ chapter_6_dictionaries/user.py
@@ -0,0 +1,9 @@
+user_0 = {
+        'username': 'tomate_cherry',
+        'first_name': 'amelie',
+        'last_name': 'Gomez Gerardi'
+        }
+
+for key, value in user_0.items():
+    print(f'\nkey: {key}')
+    print(f'value: {value}')
