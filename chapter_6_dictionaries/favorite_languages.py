5cad98fb82501dda0adec71bf894a5c771bad072
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2024-11-05 18:35:18 -0300

    Implement for loop to print sentences
    - Added a for loop to iterate through the dictionary
    - Created a formatted sentence for each key-value pair for better
    readability

--- chapter_6_dictionaries/favorite_languages.py
+++ chapter_6_dictionaries/favorite_languages.py
@@ -8,3 +8,6 @@ favorite_lenguages = {
 language = favorite_lenguages['mati'].title()
 
 print(f"Mati's favorite language is {language}")
+
+for key, value in favorite_lenguages.items():
+    print(f"\n{key.title()}'s favorite language is {value}")
