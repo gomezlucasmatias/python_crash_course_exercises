56b473a11b9ba3fb951c0b98fe9f1aac4ed977b8
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2024-11-06 22:00:05 -0300

    Add a list for additional name verification in the if statement

--- chapter_6_dictionaries/favorite_languages.py
+++ chapter_6_dictionaries/favorite_languages.py
@@ -5,9 +5,18 @@ favorite_lenguages = {
         'sabri': 'javascript',
         }
 
-language = favorite_lenguages['mati'].title()
+friends = ['mati', 'mauro']
+#language = favorite_lenguages['mati'].title()
 
-print(f"Mati's favorite language is {language}")
+#print(f"Mati's favorite language is {language}")
 
-for key, value in favorite_lenguages.items():
-    print(f"\n{key.title()}'s favorite language is {value}")
+#for key, value in favorite_lenguages.items():
+#    print(f"\n{key.title()}'s favorite language is {value}")
+
+for name in favorite_lenguages:
+    print(f'\nHi {name.title()}')
+    
+    if name in friends:
+        lenguage = favorite_lenguages[name].title()
+        print(f'\tI see that you really love {lenguage}')
+
