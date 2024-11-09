eebf284ddff99b757d7355b11555dc3fcc84f653
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2024-11-08 23:15:47 -0300

    Replace for loop for print function
    Refactor glossary printing: Use print function to display key-value
    pairs without a for loop.

--- chapter_6_dictionaries/6.3glossary.py
+++ chapter_6_dictionaries/6.3glossary.py
@@ -1,5 +1,13 @@
-glossary = {'variable': 'it is a tag poiting to a memory address', 'list': 'Store any kind of data', 'dictionary': 'Like a list, but stores key-value pair of information', 'str': 'String value', 'bool': 'True or false value'}
+glossary = {
+            'variable': 'it is a tag poiting to a memory address',
+            'list': 'Store any kind of data',
+            'dictionary': 'Like a list, but stores key-value pair of information',
+            'str': 'String value',
+            'bool': 'True or false value'
+            }
 
-for key, value in glossary.items():
-    print(f'{key.title()}: \n\t{value}')
-    print('\n')
+print(f'\nVariable: \n\t{glossary["variable"].title()}')
+print(f'\nList: \n\t{glossary["list"].title()}')
+print(f'\nDictionary: \n\t{glossary["dictionary"].title()}')
+print(f'\nstr: \n\t{glossary["str"].title()}')
+print(f'\nBool: \n\t{glossary["bool"].title()}')
---
+++ chapter_6_dictionaries/6.4glossary.py
@@ -0,0 +1,5 @@
+glossary = {'variable': 'it is a tag poiting to a memory address', 'list': 'Store any kind of data', 'dictionary': 'Like a list, but stores key-value pair of information', 'str': 'String value', 'bool': 'True or false value'}
+
+for key, value in glossary.items():
+    print(f'{key.title()}: \n\t{value}')
+    print('\n')
