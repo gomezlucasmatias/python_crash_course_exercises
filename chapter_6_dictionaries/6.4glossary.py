73927496b41cec54f9ee3eba5d95aa36e7526b07
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2024-11-08 23:43:16 -0300

    Update a dictionary with another dictionary

--- chapter_6_dictionaries/6.4glossary.py
+++ chapter_6_dictionaries/6.4glossary.py
@@ -1,5 +1,27 @@
-glossary = {'variable': 'it is a tag poiting to a memory address', 'list': 'Store any kind of data', 'dictionary': 'Like a list, but stores key-value pair of information', 'str': 'String value', 'bool': 'True or false value'}
+glossary = {
+            'variable': 'it is a tag poiting to a memory address', 
+            'list': 'Store any kind of data', 
+            'dictionary': 'Like a list, but stores key-value pair of information', 
+            'str': 'String value', 
+            'bool': 'True or false value'
+            }
+
+new_definitions = {
+                    'print': 'function that print the specified message to the sceem',
+                    'for loop': 'a way to repeat a block of code a fixed number of times',
+                    'range': 'returns a sequence of numbers',
+                    'int': 'integer numbers',
+                    'float': 'real numbers'
+                   }
 
 for key, value in glossary.items():
     print(f'{key.title()}: \n\t{value}')
     print('\n')
+
+glossary.update(new_definitions)
+
+for key, value in glossary.items():
+    print(f'{key.title()}: \n\t{value}')
+    print('\n')
+
+
