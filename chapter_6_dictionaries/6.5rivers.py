ff4a50b0bd67baf132eed09879db46da23e8c68a
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2024-11-10 20:29:54 -0300

    Make a for loop for print all key-value in sentences

--- chapter_6_dictionaries/6.5rivers.py
+++ chapter_6_dictionaries/6.5rivers.py
@@ -1,1 +1,13 @@
 rivers = {'nilo': 'egipto', 'amazonas': 'brasil', 'yangsté': 'china'}
+for key, value in rivers.items():
+    print(f'\nThe {key.title()} runs through {value.title()}')
+
+print('\nThe rivers are:')
+
+for key in rivers.keys():    
+    print(f'\n\t\t{key.title()}')
+
+print('\n\nThe countries are:')
+
+for value in rivers.values():    
+    print(f'\n\t\t{value.title()}')
