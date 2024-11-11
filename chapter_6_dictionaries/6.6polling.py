8f7ae73d2a631d73f8081501a4f4f47d628e0fb1
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2024-11-11 20:38:52 -0300

    through the list to check who is in the dictionary

--- chapter_6_dictionaries/6.6polling.py
+++ chapter_6_dictionaries/6.6polling.py
@@ -7,4 +7,10 @@ favorite_lenguages = {
                       }
 
 some_friends = ['carlas', 'gustavo', 'ivan', 'romi', 'damaris']
+
+for name in some_friends:
+    if name in favorite_lenguages.keys():
+        print(f'\n{name.title()}, thank you for take my poll!')
+    else:
+        print(f'\n{name.title()}, You should to take my poll, please')
  
