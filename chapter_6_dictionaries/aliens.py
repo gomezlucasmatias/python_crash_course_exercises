d835ebc3a42e8c705209f9d968a11a82fb1ce2e2
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2024-11-11 22:49:45 -0300

    changing values into dictionary using for loop

--- chapter_6_dictionaries/aliens.py
+++ chapter_6_dictionaries/aliens.py
@@ -20,3 +20,16 @@ for alien in aliens[0:5]:
     print(alien)
 
 print('\n...')
+print('\nChanging kinds of aliens')
+
+for alien in aliens[0:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['point'] = 10
+        alien['speed'] = 'medium'
+
+print('\n')
+
+for alien in aliens[0:5]:
+    print(alien)
+ 
