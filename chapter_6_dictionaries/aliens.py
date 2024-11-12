35f7f32564522addc07173674c90c1d6bc9904d5
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2024-11-11 21:15:35 -0300

    Creating dictionaries and nesting in a list

---
+++ chapter_6_dictionaries/aliens.py
@@ -0,0 +1,9 @@
+alien_0 = {'color': 'green', 'points': 5}
+alien_1 = {'color': 'red', 'points': 10}
+alien_2 = {'color': 'yellow', 'points': 15}
+
+aliens = [alien_0, alien_1, alien_2]
+
+for alien in aliens:
+    print(alien)
+
