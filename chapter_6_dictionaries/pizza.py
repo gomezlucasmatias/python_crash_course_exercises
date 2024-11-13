4e2ef33ff54e187c19d4270d117a47f8516d0e32
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2024-11-12 23:54:30 -0300

    to construct sentences with values from a dictionary

--- chapter_6_dictionaries/pizza.py
+++ chapter_6_dictionaries/pizza.py
@@ -3,4 +3,7 @@ pizza = {
         'toppings': ['mushrooms', 'extra cheese', 'pepperoni']
         }
 
- 
+print(f'You ordered a {pizza["crust"]}-crust pizza with the following toppings:')
+
+for topping in pizza['toppings']:
+    print('\t\t\t\t\t\t\t\t' + topping)
