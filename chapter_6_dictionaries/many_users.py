9f781c0f5c7cd420d964ae82b0d5fccd40d269fc
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2024-11-15 23:28:40 -0300

    print user information

--- chapter_6_dictionaries/many_users.py
+++ chapter_6_dictionaries/many_users.py
@@ -1,3 +1,11 @@
 users = {'aeinstein': {'name': 'albert', 'lastname': 'einstein', 'location': 'princeston',},
          'mcurie': {'name': 'marie', 'lastname': 'curie', 'location': 'paris',}
          }
+
+for username, user_info in users.items():
+    print(f'\nUsername: {username}')
+    full_name = f'{user_info['name']} {user_info['lastname']}'
+    location = f'{user_info["location"]}'
+
+    print(f"\tFull name: {full_name.title()}")
+    print(f"\tlocation: {location.title()}")
