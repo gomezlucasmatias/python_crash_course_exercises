368bdbdd8244012adf2b6ee187f6f5dcc6875c5b
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2024-11-17 23:24:27 -0300

    make darian's dicctionary

--- chapter_6_dictionaries/6.7people.py
+++ chapter_6_dictionaries/6.7people.py
@@ -1,3 +1,7 @@
 ame = {'name': 'ame', 'last_name': 'gomez gerardi', 'age': '11',}
 
 mati = {'name': 'mati', 'last_name': 'gomez', 'age': '37',}
+
+darian = {'name': 'darian', 'last_name': 'gomez gerardi', 'age': '17',}
+
+
