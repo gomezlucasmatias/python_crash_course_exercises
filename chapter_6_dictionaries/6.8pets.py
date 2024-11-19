c04c5fd64fb076ef6dec7e5c084c933cc1de2c59
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2024-11-19 19:51:06 -0300

    making a few dictionaries

---
+++ chapter_6_dictionaries/6.8pets.py
@@ -0,0 +1,7 @@
+artemisa = {'kind': 'cat', 'owner': 'ame',}
+
+felipe = {'kind': 'cat', 'owner': 'luz'}
+
+octabio = {'kind': 'cat', 'owner': 'damaris'}
+
+aurelia = {'kind': 'dog', 'owner': 'lili'}
