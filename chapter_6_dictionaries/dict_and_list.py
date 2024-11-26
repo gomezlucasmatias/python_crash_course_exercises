e0c6ceb237ce10f2b71f714319121b1672178856
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2024-11-26 20:45:35 -0300

    making a dictionary

---
+++ chapter_6_dictionaries/dict_and_list.py
@@ -0,0 +1,5 @@
+list_corvidos = {
+                'trails': ['voge dsx 900', 'voge dsx 650', 'voge dsx 525',], 
+                'enduro': ['royal enfield himalayan 450', 'voge 300 rally', 'cfmoto 450 mt',], 
+                'others': ['zontes 703f', 'benelli trk 702 x', 'cfmoto 650 gt',]
+                }
