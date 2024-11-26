443295f845041f162c266914db317dd0b4915ae6
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2024-11-25 23:40:56 -0300

    Making a dicctionary

---
+++ chapter_6_dictionaries/6.9favorite_places.py
@@ -0,0 +1,7 @@
+favorite_places = {
+                   'darian': ['rusia', 'paraguay', 'new zeland'],
+                   'ame': ['korea', 'japon', 'mexico'],
+                   'artemisa': ['window', 'stairs', 'yard'],
+                   }
+
+
