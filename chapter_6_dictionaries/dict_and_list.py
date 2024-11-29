25b98802e1743217e2b451180ad333d741f4bd67
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2024-11-28 22:10:57 -0300

    Practice

--- chapter_6_dictionaries/dict_and_list.py
+++ chapter_6_dictionaries/dict_and_list.py
@@ -3,3 +3,8 @@ list_corvidos = {
                 'enduro': ['royal enfield himalayan 450', 'voge 300 rally', 'cfmoto 450 mt',], 
                 'others': ['zontes 703f', 'benelli trk 702 x', 'cfmoto 650 gt',]
                 }
+
+for kind, motos in list_corvidos.items():
+    print(f"{kind.title()}:")
+    for moto in motos:
+        print(f"\n\t\t\t{moto.title()}")
